8fe1eb970d03e049a8e29fdcd0a6ad21bf2540ba
Capu <capucine.fortin@epita.fr>
2023-04-23 17:49:42 +0200

    [INIT] 1st part of project, took from Shalimao

---
+++ lib/Home/view/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../controller/qrScan.dart';
+
+class HomeWidget extends StatelessWidget {
+  String qrValue;
+  HomeWidget({Key? key, required this.qrValue}) : super (key: key);
+
+
+  @override
+  Widget build(BuildContext context){
+    return Column(children: [
+      const SizedBox(width: 500, height: 500),
+      ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const QRReader(),
+            ));},
+          child: Text('Scan QR code')),
+      Text(qrValue),
+      const SizedBox(
+        height: 70,
+      ),
+      TextField(),
+      const SizedBox(
+        height: 30,
+      ),
+      ElevatedButton(
+        onPressed: () => {},
+        child:
+          const Text("Valider")
+      ),
+    ]
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:proloapp/Home/view/home.dart';
+
+import 'Leaderboard/view/leaderboard.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(qrValue: ''),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.qrValue}) : super(key: key);
+
+  final String qrValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        home: DefaultTabController(
+            initialIndex: 1,
+            length: 3,
+            child: Scaffold(
+                bottomNavigationBar: const BottomAppBar(
+                  child : TabBar(
+                    labelColor: Colors.black,
+                    tabs: [
+                      Tab(icon: Icon(Icons.schedule)),
+                      Tab(icon: Icon(Icons.home)),
+                      Tab(icon: Icon(Icons.person)),
+                    ],
+                  ),
+                ),
+                body: TabBarView(
+                    children: [
+                      LeaderboardWidget(),
+                      HomeWidget(qrValue: qrValue),
+                    ])
+            )
+        )
+    );
+  }
+}
