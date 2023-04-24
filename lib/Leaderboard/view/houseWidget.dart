fb6d7029d6a606166b8798aec93c81981c5963c1
Capu <capucine.fortin@epita.fr>
2023-04-24 22:19:08 +0200

    feature: kinda prettier house leaderboard

--- lib/Leaderboard/view/houseWidget.dart
+++ lib/Leaderboard/view/houseWidget.dart
@@ -1,19 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:proloapp/Leaderboard/model/name.dart';
-
+import '../controller/houseController.dart';
 import '../model/house.dart';
+import 'package:flutter/material.dart';
 
-Widget getTeamWidget(House house){
+Color getColor(int index) {
+  switch (index) {
+    case 0:
+      return const Color(0xFFFFD700);
+    case 1:
+      return const Color(0xFFC0C0C0);
+    case 2:
+      return const Color(0xFFCD7F32);
+    default:
+      return Colors.black;
+  }
+}
+
+Widget getTeamWidget(int index){
+  House house = sortedHouse()[index];
+  Color color = getColor(index);
+
   return Container(
-      height: 50,
-      width: 60,
+      height: 100,
+      padding: const EdgeInsets.all(30),
       decoration: BoxDecoration(
-          color: house.color,
+        color: house.color,
+        borderRadius: BorderRadius.circular(30)
       ),
-      child: Column(
+      child: Row(
           children: [
-            Text(house.name.stringify()),
-            Text(house.points.toString()),
+            Text(
+              (index+1).toString(),
+              style: TextStyle(
+                fontSize: 20,
+                color: color,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Icon(
+                Icons.emoji_events,
+                color: color
+            ),
+            Text(
+                house.name.stringify(),
+                  style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                )
+            ),
+            Text(
+                house.points.toString(),
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                )
+            ),
           ]
       )
   );
--- lib/Leaderboard/view/leaderboard.dart
+++ lib/Leaderboard/view/leaderboard.dart
@@ -6,14 +6,20 @@ import '../model/house.dart';
 class LeaderboardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<House> houses = sortedHouse();
-    return Center (
-        child: ListView.builder(
-        itemCount: 4,
-        itemBuilder: (BuildContext context, int index) {
-          return getTeamWidget(houses[index]);
-        }
-    )
+    return Center(child:
+        SizedBox(
+          height: 500,
+          child: ListView.separated(
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: 4,
+            itemBuilder: (BuildContext context, int index) {
+              return getTeamWidget(index);
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 20);
+            },
+          )
+        )
     );
   }
 }
