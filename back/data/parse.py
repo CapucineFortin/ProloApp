43416d90bb42f33537903ff424116fc2cb08b25d
Miyaou <melanie.tcheou@gmail.com>
2023-04-26 14:16:43 +0200

    chore: setup with readme

---
+++ back/data/parse.py
@@ -0,0 +1,21 @@
+import csv
+from users_manager.models import Contestant, House, User
+import random
+
+def import_all():
+    houses = ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]
+    for house in houses:
+        House.objects.get_or_create(name=house)
+
+    with open('data/contestants.csv', newline='') as csvfile:
+        reader=csv.DictReader(csvfile, fieldnames=['username', 'lastname', 'firstname', 'is_staff'])
+        for row in reader:
+            user = User.objects.create(username=row['username'],
+                                        first_name= row["firstname"],
+                                        last_name= row["lastname"],
+                                        email = f'{row["username"]}@contest.idk',
+                                        is_staff = row["is_staff"])
+            if row["is_staff"] == "0":
+                house, _ = House.objects.get_or_create(name=houses[random.randint(0, 3)])
+                contestant = Contestant.objects.create(user=user, house=house, score=0)
+                contestant.save()
