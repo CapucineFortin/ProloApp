05711d1c34719c02eb2cfd01678e483bff7332aa
Miyaou <melanie.tcheou@gmail.com>
2023-05-18 18:22:14 +0200

    data: add orgas meals

--- back/data/meals.py
+++ back/data/meals.py
@@ -14,14 +14,15 @@ def contestants(day=0, meal_type=3):
                 print(row['login'])
                 continue
 
-def orgas(day, meal_type):
+def orgas(day=0, meal_type=3):
     with open('data/orgas_meal.csv', newline='') as csvfile:
         reader=csv.DictReader(csvfile, fieldnames=['login', 'lastname', 'firstname', 'eating'])
         for row in reader:
-            try:
-                user = ProloginUser.objects.get(username=row['login'])
-                meal = Meal.objects.create(user=user, day=0, meal_type=3)
-                meal.save()
-            except:
-                print(row['login'])
-                continue
+            if row['eating'] == "OUI":
+                try:
+                    user = ProloginUser.objects.get(username=row['login'])
+                    meal = Meal.objects.create(user=user, day=day, meal_type=meal_type)
+                    meal.save()
+                except:
+                    print(row['login'])
+                    continue
