cfd60496d24c1cec1e248d24cb68f51b1a0d6858
Miyaou <melanie.tcheou@gmail.com>
2023-05-18 18:24:24 +0200

    back: add reset meals

--- back/data/all.py
+++ back/data/all.py
@@ -1,6 +1,12 @@
 from data.parse import go
-from data.meals import contestants
+from data.meals import contestants, orgas
+from finale.models import Meal
 
 def all():
     go()
     contestants(0, 3)
+
+def meals():
+    Meal.objects.all().delete()
+    contestants(day=0, meal_type=3)
+    orgas(day=0, meal_type=3)
