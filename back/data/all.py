310bd06f52ebdff02e01edffdd3696a3229a6565
Miyaou <melanie.tcheou@gmail.com>
2023-05-19 09:14:50 +0200

    back: reset scores script

--- back/data/all.py
+++ back/data/all.py
@@ -1,10 +1,13 @@
 from data.parse import go
 from data.meals import contestants, orgas
-from finale.models import Meal
+from finale.models import Meal, Score
 
 def all():
     go()
     contestants(0, 3)
+    
+def reset_scores():
+    Score.objects.all().delete()
 
 def meals():
     Meal.objects.all().delete()
