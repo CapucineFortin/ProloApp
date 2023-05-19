25908e41176be890323706902a0ff82d6c7b18c4
Miyaou <melanie.tcheou@gmail.com>
2023-05-19 09:32:10 +0200

    back: admin display readable

--- back/finale/admin.py
+++ back/finale/admin.py
@@ -7,7 +7,27 @@ class ContestantAdmin(admin.ModelAdmin):
 
     def contestant(self, obj):
         return (obj.user.username)
+    
+class MealAdmin(admin.ModelAdmin):
+    list_filter = ('day', 'meal_type', 'eaten', 'user__is_staff')
+    list_display = ('get_username', 'day', 'meal_type', 'eaten')
+    
+    @admin.display(ordering='user__name', description='User name')
+    def get_username(self, obj):
+        return obj.user.username
+    
+class ScoreAdmin(admin.ModelAdmin):
+    list_filter = ('category', 'contestant__house')
+    list_display = ('get_contestant_name', 'get_contestant_house', 'category', 'points')
+    
+    @admin.display(ordering='score_id', description='Contestant user username')
+    def get_contestant_name(self, obj):
+        return obj.contestant.user.username
+    
+    @admin.display(ordering='score_id', description='Contestant house')
+    def get_contestant_house(self, obj):
+        return obj.contestant.get_house_display()
 
 admin.site.register(Contestant, ContestantAdmin)
-admin.site.register(Score)
-admin.site.register(Meal)
+admin.site.register(Score, ScoreAdmin)
+admin.site.register(Meal, MealAdmin)
