0fce98765c695dbaa76152753517dd2cda8c8648
Miyaou <melanie.tcheou@gmail.com>
2023-05-19 09:15:11 +0200

    back: setup for day1 lunch

--- back/data/meals.py
+++ back/data/meals.py
@@ -2,7 +2,7 @@ from users_manager.models import ProloginUser
 from finale.models import Meal
 import csv
 
-def contestants(day=0, meal_type=3):
+def contestants(day=1, meal_type=2):
     with open('data/contestants.csv', newline='') as csvfile:
         reader=csv.DictReader(csvfile, fieldnames=['login', 'lastname', 'firstname'])
         for row in reader:
@@ -14,15 +14,14 @@ def contestants(day=0, meal_type=3):
                 print(row['login'])
                 continue
 
-def orgas(day=0, meal_type=3):
-    with open('data/orgas_meal.csv', newline='') as csvfile:
-        reader=csv.DictReader(csvfile, fieldnames=['login', 'lastname', 'firstname', 'eating'])
+def orgas(day=1, meal_type=2):
+    with open('data/orgas.csv', newline='') as csvfile:
+        reader=csv.DictReader(csvfile, fieldnames=['login', 'lastname', 'firstname'])
         for row in reader:
-            if row['eating'] == "NON":
-                try:
-                    user = ProloginUser.objects.get(username=row['login'])
-                    meal = Meal.objects.create(user=user, day=day, meal_type=meal_type)
-                    meal.save()
-                except:
-                    print(row['login'])
-                    continue
+            try:
+                user = ProloginUser.objects.get(username=row['login'])
+                meal = Meal.objects.create(user=user, day=day, meal_type=meal_type)
+                meal.save()
+            except:
+                print(row['login'])
+                continue
